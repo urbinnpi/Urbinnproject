3aabbc3e8e2e453f1bc25564a4e22fd383f3e3f2
urbinnpi <37292633+urbinnpi@users.noreply.github.com>
2018-03-26 12:55:44 +0200

    Add files via upload

---
+++ your_workspace_name_here/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/pi/your_workspace_name_here/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/pi/your_workspace_name_here/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/pi/your_workspace_name_here/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD="/home/pi/your_workspace_name_here/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/pi/your_workspace_name_here/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/pi/your_workspace_name_here/src:$ROS_PACKAGE_PATH"
---
+++ your_workspace_name_here/build/your_package_name/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/pi/your_workspace_name_here/src/your_package_name
+# Build directory: /home/pi/your_workspace_name_here/build/your_package_name
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
