3a152d1fac06e6a5f59fd80a3003c2f34e20672b
urbinnpi <37292633+urbinnpi@users.noreply.github.com>
2018-03-26 13:08:40 +0200

    Add files via upload

---
+++ your_workspace_name_here/build/CMakeFiles/listener.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/listener.dir/subscriber.cpp.o"
+  "/home/pi/your_workspace_name_here/devel/lib/beginner_tutorials/listener.pdb"
+  "/home/pi/your_workspace_name_here/devel/lib/beginner_tutorials/listener"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/listener.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
