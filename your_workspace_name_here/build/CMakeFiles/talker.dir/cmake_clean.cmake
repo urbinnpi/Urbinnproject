a2574cff7f267f96f976cb500ac35240eef38b2c
urbinnpi <37292633+urbinnpi@users.noreply.github.com>
2018-03-26 13:06:34 +0200

    Add files via upload

---
+++ your_workspace_name_here/build/CMakeFiles/talker.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/talker.dir/publisher.cpp.o"
+  "/home/pi/your_workspace_name_here/devel/lib/beginner_tutorials/talker.pdb"
+  "/home/pi/your_workspace_name_here/devel/lib/beginner_tutorials/talker"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/talker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
