6b0186d28e1d2798952228b9c9ae86c44ff47df2
Lucan de Groot <Lucan@e-trailer.nl>
2018-04-25 14:47:54 +0200

    created atmel studio project for the Arduino

---
+++ C code/URBINN_ARDUINO/main.c
@@ -0,0 +1,18 @@
+/*
+ * URBINN_ARDUINO.c
+ *
+ * Created: 25-4-2018 14:46:07
+ * Author : lucan
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
