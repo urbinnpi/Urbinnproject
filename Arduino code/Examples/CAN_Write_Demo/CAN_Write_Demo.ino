852e6e2e6b2a5ad6f334976c3773166cfd6756e6
Lucan de Groot <Lucan@e-trailer.nl>
2018-04-25 14:02:23 +0200

    forgot to save this file

--- Arduino code/Examples/CAN_Write_Demo/CAN_Write_Demo.ino
+++ Arduino code/Examples/CAN_Write_Demo/CAN_Write_Demo.ino
@@ -33,12 +33,12 @@ void setup() {
 
 void loop() 
 {
-tCAN message;
+  tCAN message;
 
-        message.id = 0x631; //formatted in HEX
-        message.header.rtr = 0;
-        message.header.length = 8; //formatted in DEC
-        message.data[0] = 0x40;
+  message.id = 0x631; //formatted in HEX
+  message.header.rtr = 0;
+  message.header.length = 8; //formatted in DEC
+  message.data[0] = 0x40;
 	message.data[1] = 0x05;
 	message.data[2] = 0x30;
 	message.data[3] = 0xFF; //formatted in HEX
@@ -47,8 +47,8 @@ tCAN message;
 	message.data[6] = 0x00;
 	message.data[7] = 0x00;
 
-mcp2515_bit_modify(CANCTRL, (1<<REQOP2)|(1<<REQOP1)|(1<<REQOP0), 0);
-mcp2515_send_message(&message);
-
-delay(1000);
+  mcp2515_bit_modify(CANCTRL, (1<<REQOP2)|(1<<REQOP1)|(1<<REQOP0), 0);
+  mcp2515_send_message(&message);
+  
+  delay(1000);
 }
