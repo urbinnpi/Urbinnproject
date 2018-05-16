95dbfbaf46bbe5e6bc1e7b7602e72e61eedc03e4
dettotje <ferrydegroot16@outlook.com>
2018-05-16 10:58:45 +0200

    made driver.h according to model

--- RaspberryPi/driver.h
+++ RaspberryPi/driver.h
@@ -1,4 +1,15 @@
 #ifndef DRIVER_H
 #define DRIVER_H
 
+
+class Driver{
+public:
+    Driver(){}
+    ~Driver(){}
+        virtual void readinput() =0;
+        virtual void receivemsg()=0;
+        virtual void transmitMsg(messageStruct)=0;
+  private:
+
+}
 #endif // DRIVER_H
