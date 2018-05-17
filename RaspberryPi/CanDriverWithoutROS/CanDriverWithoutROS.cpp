7b11e1d17bae23a61facfec6eaeb300870378d6a
Lucan de Groot <lucan.degroot@gmail.com>
2018-05-17 16:23:48 +0200

    removed spaces in data

--- RaspberryPi/CanDriverWithoutROS/CanDriverWithoutROS.cpp
+++ RaspberryPi/CanDriverWithoutROS/CanDriverWithoutROS.cpp
@@ -49,7 +49,7 @@ void receive(struct can_frame *frame, int socket){
 			// loop trough the data
 			for(uint8_t i = 0; frame->can_dlc > i; i++) {
 				//std::cout << " " << std::uppercase << std::hex << (unsigned int)frame->data[i];
-				std::cout << " " << (char)frame->data[i];
+				std::cout << (char)frame->data[i];
 			}
 			
 			// end of frame
