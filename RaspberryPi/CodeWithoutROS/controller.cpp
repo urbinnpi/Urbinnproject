5608973e04be9d275e54099563f9a06c4fc9234d
Jasper <14083795@student.hhs.nl>
2018-05-22 13:57:30 +0200

    Debug

--- RaspberryPi/CodeWithoutROS/controller.cpp
+++ RaspberryPi/CodeWithoutROS/controller.cpp
@@ -2,19 +2,22 @@
 #include "controller.h"
 #include "uartdriver.h"
 
-Controller::Controller() : ud1(new UARTdriver()) {
+/*Controller::Controller() : ud1(new UARTdriver()) {
 	
 }
 
 Controller::~Controller() {
 	delete ud1;
-}
+}*/
 
 void Controller::receiveInfo(struct can_frame *frame) {
 	// Lees infostruct uit en voer aan de hand daarvan functies zoals steer of brake uit
 	// Deze functies kunnen vervolgens messages sturen naar de driver met transmitMsg()
-	frame->can_id += 1;
-	this->transmitMsg(frame);
+	frame->data[0] += 1;
+	frame->data[1] += 1;
+	//this->transmitMsg(frame);
+	int nbytes;
+	nbytes = write(s, frame, sizeof(struct can_frame)); // Write the frame
 
 	/*
 	std::cout << "ID: " << std::uppercase << std::hex << (unsigned int)frame->can_id << " Length: " << (unsigned int)frame->can_dlc << " Data: ";
@@ -30,5 +33,5 @@ void Controller::receiveInfo(struct can_frame *frame) {
 
 void Controller::transmitMsg(struct can_frame *frame) {
 	// Bij gebruik ROS hier msgStruct publishen op topic ControllerDriver1
-	ud1->receiveMsg(frame); // Tijdelijk gebruik van driver callback
+	//ud1->receiveMsg(frame); // Tijdelijk gebruik van driver callback
 }
