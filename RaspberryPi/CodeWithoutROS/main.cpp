5a50b42b0b1b19acc006e425a712444cc30ee6c5
Jasper <14083795@student.hhs.nl>
2018-05-22 11:23:18 +0200

    Threads added

--- RaspberryPi/CodeWithoutROS/main.cpp
+++ RaspberryPi/CodeWithoutROS/main.cpp
@@ -1,5 +1,5 @@
 /*
- * Compile with: g++ main.cpp -o ProgramV1 -std=c++11 -pthread
+ * Compile with: g++ *.cpp -o ProgramV1 -std=c++11 -pthread
  */
 
 #include <unistd.h>
@@ -24,15 +24,19 @@ int main(void) {
 	struct can_frame frame;
 	struct can_frame frame2;
 
-	UARTdriver ud1;
-	
+	//UARTdriver ud1;
+	UARTdriver* ud1 = new UARTdriver();
+
 	//std::thread t1(ud1.readInput);//, &frame);
 	//std::thread t2(ud1.transmit);//, &frame2);
+	std::thread t1(&UARTdriver::readInput, ud1, &frame);
+	std::thread t2(&UARTdriver::transmit, ud1, &frame2);
 	
-	while(1) {
-		ud1.readInput(&frame);
-		ud1.transmit(&frame2);
-	}
+	//while(1) {
+	//	ud1.transmit(&frame2);
+	//	ud1.readInput(&frame);
+	//}
 	
+	delete ud1;
 	return 0;
 }
--- RaspberryPi/CodeWithoutROS/uartdriver.cpp
+++ RaspberryPi/CodeWithoutROS/uartdriver.cpp
@@ -26,20 +26,22 @@ UARTdriver::UARTdriver(): p(new UARTparser) {
 }
 
 void UARTdriver::readInput(struct can_frame *frame) {
-	int recvbytes = read(s, frame, sizeof(struct can_frame));
+	while(1) {
+		int recvbytes = read(s, frame, sizeof(struct can_frame));
 
-	/*if(recvbytes) {
-		std::cout << "ID: " << std::uppercase << std::hex << (unsigned int)frame->can_id << " Length: " << (unsigned int)frame->can_dlc << " Data: ";
+		/*if(recvbytes) {
+			std::cout << "ID: " << std::uppercase << std::hex << (unsigned int)frame->can_id << " Length: " << (unsigned int)frame->can_dlc << " Data: ";
 		
-		// Loop trough the data
-		for(uint8_t i = 0; frame->can_dlc > i; i++) {
-			//std::cout << " " << std::uppercase << std::hex << (unsigned int)frame->data[i];
-			std::cout << " " << (char)frame->data[i];
-		}
+			// Loop trough the data
+			for(uint8_t i = 0; frame->can_dlc > i; i++) {
+				//std::cout << " " << std::uppercase << std::hex << (unsigned int)frame->data[i];
+				std::cout << " " << (char)frame->data[i];
+			}
 		
-		std::cout << std::endl; // End of frame
-	}*/
-	if(recvbytes) p->receiveMsg(frame); // Tijdelijk gebruik van parser callback
+			std::cout << std::endl; // End of frame
+		}*/
+		if(recvbytes) p->receiveMsg(frame); // Tijdelijk gebruik van parser callback
+	}
 }
 
 void UARTdriver::receiveMsg() {
@@ -48,14 +50,16 @@ void UARTdriver::receiveMsg() {
 
 void UARTdriver::transmit(struct can_frame *frame){
 	int nbytes, id;
-	std::cin >> id;
+	while(1) {
+		std::cin >> id;
 
-	// Create the frame
-	//frame->can_id  = 0x123;
-	frame->can_id = id;
-	frame->can_dlc = 2;
-	frame->data[0] = 0x11;
-	frame->data[1] = 0x22;
+		// Create the frame
+		//frame->can_id  = 0x123;
+		frame->can_id = id;
+		frame->can_dlc = 2;
+		frame->data[0] = 0x11;
+		frame->data[1] = 0x22;
 
-	nbytes = write(s, frame, sizeof(struct can_frame)); // Write the frame
-} 
+		nbytes = write(s, frame, sizeof(struct can_frame)); // Write the frame
+	}
+}
