7ef365efb8b0a51d97bb5f9117fd03f2da5b65cc
Jasper <14083795@student.hhs.nl>
2018-06-07 21:53:09 +0200

    Debug

--- RaspberryPi/CodeWithROS/communication/src/sensorxparser.h
+++ RaspberryPi/CodeWithROS/communication/src/sensorxparser.h
@@ -17,10 +17,24 @@
 class SensorXparser : public Parser
 {
 public:
-	SensorXparser(ros::Publisher* pub);
-	void parseData(const communication::msgStruct msg);
+	SensorXparser(ros::Publisher* pub) : pub(pub) { ROS_INFO("started SensorXparser"); }
+	void parseData(const communication::msgStruct msg) { 
+		// Translate data from msgStruct to usable data in an infoStruct
+		ROS_INFO("SensorXparser got frame to parse");
+		communication::infoStruct info;
+		info.id = msg.id;
+		info.dl = msg.dl;
+		for(uint8_t i = 0; info.dl > i; i++) {
+			info.data[i] = msg.data[i];
+		}
+		ROS_INFO("SensorXparser sending info to controller");
+
+		this->transmitInfo(info);
+	}
 private:
-	void transmitInfo(communication::infoStruct info);
+	void transmitInfo(communication::infoStruct info) {
+		pub->publish(info);
+	}
 	ros::Publisher* pub;
 };
 
