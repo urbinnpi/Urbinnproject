6483f706cb0ce0d9f41d4d9e8d8aa91b47ac4e5e
Lucan de Groot <lucan.degroot@gmail.com>
2018-06-07 10:44:40 +0200

    added sensor y parser

---
+++ RaspberryPi/CodeWithROS/communication/src/sensoryparser.h
@@ -0,0 +1,39 @@
+#ifndef SENSORYPARSER_H
+#define SENSORYPARSER_H
+
+#include <unistd.h>
+#include <string.h>
+#include <net/if.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <map>
+#include "socketcan/can.h"
+#include "parser.h"
+#include "ros/ros.h"
+#include "communication/msgStruct.h"
+#include "communication/infoStruct.h"
+
+class Controller;
+
+class SensorYparser : public Parser
+{
+public:
+	SensorYparser(ros::Publisher* pub) : pub(pub) { ROS_INFO("started SensorYparser"); }
+	void parseData(const communication::msgStruct msg) {
+		ROS_INFO("SensorYparser Got frame to parse");
+		communication::infoStruct info;
+		info.id = msg.id;
+		info.dl = msg.dl;
+		for(uint8_t i = 0; info.dl > i; i++) {
+			info.data[i] = msg.data[i];
+		}
+		ROS_INFO("SensorYparser sending info to controller");
+		pub->publish(info);
+	}
+private:
+	void transmitInfo(communication::infoStruct info) { pub->publish(info); }
+	ros::Publisher* pub;
+};
+
+#endif // SENSORYPARSER_H
